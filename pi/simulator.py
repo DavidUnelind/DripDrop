9f673bb5cf8ea1389379487eb67517e0577d6a60
johannafridh <113369299+johannafridh@users.noreply.github.com>
2023-03-29 14:52:02 +0200

    Add files via upload

---
+++ pi/simulator.py
@@ -0,0 +1,81 @@
+import math
+import requests
+import argparse
+
+def getMovement(src, dst):
+    speed = 0.00001
+    dst_x, dst_y = dst
+    x, y = src
+    direction = math.sqrt((dst_x - x)**2 + (dst_y - y)**2)
+    longitude_move = speed * ((dst_x - x) / direction )
+    latitude_move = speed * ((dst_y - y) / direction )
+    return longitude_move, latitude_move
+
+def moveDrone(src, d_long, d_la):
+    x, y = src
+    x = x + d_long
+    y = y + d_la        
+    return (x, y)
+
+def run(id, current_coords, from_coords, to_coords, SERVER_URL):
+    drone_coords = current_coords
+    d_long, d_la =  getMovement(drone_coords, from_coords)
+    while ((from_coords[0] - drone_coords[0])**2 + (from_coords[1] - drone_coords[1])**2)*10**6 > 0.0002:
+        drone_coords = moveDrone(drone_coords, d_long, d_la)
+        with requests.Session() as session:
+            drone_info = {'id': id,
+                          'longitude': drone_coords[0],
+                          'latitude': drone_coords[1],
+                          'status': 'busy'
+                        }
+            resp = session.post(SERVER_URL, json=drone_info)
+    d_long, d_la =  getMovement(drone_coords, to_coords)
+    while ((to_coords[0] - drone_coords[0])**2 + (to_coords[1] - drone_coords[1])**2)*10**6 > 0.0002:
+        drone_coords = moveDrone(drone_coords, d_long, d_la)
+        with requests.Session() as session:
+            drone_info = {'id': id,
+                          'longitude': drone_coords[0],
+                          'latitude': drone_coords[1],
+                          'status': 'busy'
+                        }
+            resp = session.post(SERVER_URL, json=drone_info)
+    with requests.Session() as session:
+            drone_info = {'id': id,
+                          'longitude': drone_coords[0],
+                          'latitude': drone_coords[1],
+                          'status': 'idle'
+                         }
+            resp = session.post(SERVER_URL, json=drone_info)
+    return drone_coords[0], drone_coords[1]
+   
+if __name__ == "__main__":
+    # Fill in the IP address of server, in order to location of the drone to the SERVER
+    #===================================================================
+    SERVER_URL = "http://192.168.1.3:5001/drone"
+    #===================================================================
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--clong", help='current longitude of drone location' ,type=float)
+    parser.add_argument("--clat", help='current latitude of drone location',type=float)
+    parser.add_argument("--flong", help='longitude of input [from address]',type=float)
+    parser.add_argument("--flat", help='latitude of input [from address]' ,type=float)
+    parser.add_argument("--tlong", help ='longitude of input [to address]' ,type=float)
+    parser.add_argument("--tlat", help ='latitude of input [to address]' ,type=float)
+    parser.add_argument("--id", help ='drones ID' ,type=str)
+    args = parser.parse_args()
+
+    current_coords = (args.clong, args.clat)
+    from_coords = (args.flong, args.flat)
+    to_coords = (args.tlong, args.tlat)
+
+    print(current_coords, from_coords, to_coords)
+    drone_long, drone_lat = run(args.id ,current_coords, from_coords, to_coords, SERVER_URL)
+    # drone_long and drone_lat is the final location when drlivery is completed, find a way save the value, and use it for the initial coordinates of next delivery
+    #=============================================================================
+    #my_chords = open('longlat.txt', 'w')
+    #my_chords.WriteLine(drone_long)
+    #my_chords.WriteLine(drone_lat)
+    file = open('longlat.txt', 'w')
+    file.write(str(drone_long) + "\n" + str(drone_lat))
+    file.close()
+    
