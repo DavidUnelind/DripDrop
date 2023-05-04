16fecc8c0b08b716038e437ba719bb447a7bf0df
NaiKha <naimakh971@gmail.com>
2023-05-04 13:51:05 +0200

    Update website 2.0

--- webserver/build.py
+++ webserver/build.py
@@ -39,6 +39,14 @@ def translate(coords_osm):
 @app.route('/', methods=['GET'])
 def map():
     return render_template('index.html')
+    
+@app.route('/about')
+def about():
+    return render_template('about.html')
+    
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 @app.route('/get_drones', methods=['GET'])
 def get_drones():
